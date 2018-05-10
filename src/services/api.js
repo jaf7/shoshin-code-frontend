21c38a04a3ba43f7772c1edc03f1b7598acc3320
jaf7 <janthonyfields@gmail.com>
2018-05-09 20:46:05 -0400

    change to secure WebSocket endpoint wss

--- src/services/api.js
+++ src/services/api.js
@@ -15,7 +15,7 @@
 // 6. run npm install && npm start in the frontend
 
 export const API_ROOT = 'https://shoshin-code-backend.herokuapp.com'
-export const API_WS_ROOT = 'ws://shoshin-code-backend.herokuapp.com/cable' 
+export const API_WS_ROOT = 'wss://shoshin-code-backend.herokuapp.com/cable' 
 
 // export const API_ROOT = 'http://localhost:3001'
 // export const API_WS_ROOT = 'ws://localhost:3001/cable' 
