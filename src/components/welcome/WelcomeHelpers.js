953012234a2b31478f5e9cee1ec512337bab91e7
jaf7 <janthonyfields@gmail.com>
2018-05-10 11:00:15 -0400

    styling welcome pages

--- src/components/welcome/WelcomeHelpers.js
+++ src/components/welcome/WelcomeHelpers.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import MaterialIcon, {colorPallet} from 'material-icons-react'
 import LoginSignup from '../auth/LoginSignup'
 
 export const WelcomeMessage = (props) => {
@@ -16,8 +17,11 @@ export const LoggedInMessage = (props) => {
   return (
     <div>
     <h1 className="welcome-heading">Welcome To Shoshin Code</h1>
-      <p>Welcome {props.username}. You are a problem solver. Ready for anything, open to everything. Engage your beginner's mind.</p>
-      <p>[brief instructions]</p>
+      <p className="welcome-welcome">Welcome {props.username}. Here is a space for you to engage a growth mindset, a beginner's mind. </p>
+
+      <p className="welcome-instructions">"Exercises" and "Code It" will take you to coding puzzles and exercises to choose from, and to the editor and repl, respectively.
+      The left menu is where you can save {<MaterialIcon icon="save" color={colorPallet.blueGrey._500} />} and view your saved exercises. You can come back anytime to continue progress on any exercise, just select it from the list.
+      The share {<MaterialIcon icon="share" color={colorPallet.blueGrey._500} />} button will provide a link you can send to a friend / mentor / interviewer. This will land them at the same exercise view with a read-only version of your editor, so they can view your progress. (Chat coming in the next version)</p>
     </div>
   )
 }
